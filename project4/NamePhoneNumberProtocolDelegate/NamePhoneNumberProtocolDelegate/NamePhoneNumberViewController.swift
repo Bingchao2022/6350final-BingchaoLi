f508c551eb6d75442f22c505dfd10a588812046e
Bingchao Li <li.bingch@northeastern.edu>
2023-12-14 18:59:08 -0800

    Your commit message

---
+++ project4/NamePhoneNumberProtocolDelegate/NamePhoneNumberProtocolDelegate/NamePhoneNumberViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NamePhoneNumberViewController.swift
+//  NamePhoneNumberProtocolDelegate
+//
+//  Created by 李炳超 on 12/14/23.
+//
+
+import UIKit
+
+class NamePhoneNumberViewController: UIViewController {
+
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var name: UITextField!
+    
+    var sendNamePhoneDelegate: SendNamePhoneNumberDelegate?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveNamePhone(_ sender: Any) {
+        guard let name = name.text else {return}
+        guard let phoneNumber = phoneNumber.text else {return}
+        
+        sendNamePhoneDelegate?.sendNamePhoneNumber(name: name, phoneNumber: phoneNumber)
+        
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+}
---
+++ project4/NamePhoneNumberProtocolDelegate/NamePhoneNumberProtocolDelegate/SendNamePhoneNumberDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SendNamePhoneNumberdelegate.swift
+//  NamePhoneNumberProtocolDelegate
+//
+//  Created by 李炳超 on 12/14/23.
+//
+
+import Foundation
+
+protocol SendNamePhoneNumberDelegate{
+    func sendNamePhoneNumber(name: String, phoneNumber: String)
+}
